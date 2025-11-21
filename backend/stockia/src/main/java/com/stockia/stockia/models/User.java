679ee3c6e215f26c8ea554809176b19bd82ef9ca
Leandro74377 <leandrobaque4@gmail.com>
2025-11-21 00:52:47 -0500

    Update User.java

--- backend/stockia/src/main/java/com/stockia/stockia/models/User.java
+++ backend/stockia/src/main/java/com/stockia/stockia/models/User.java
@@ -1,35 +1,5 @@
 package com.stockia.stockia.models;
 
-import jakarta.persistence.*; 
+public class {
 
-@Entity
-@Table(name = "clientes")
-public class Cliente {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String nombre;
-
-    @Column(unique = true, nullable = false)
-    private String correoElectronico; 
-
-    @Column(unique = true, nullable = false)
-    private String telefono; 
-
-    private Boolean clienteFrecuente;
-
-    
-    public Cliente() {} 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getNombre() { return nombre; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
-    public String getCorreoElectronico() { return correoElectronico; }
-    public void setCorreoElectronico(String correoElectronico) { this.correoElectronico = correoElectronico; }
-    public String getTelefono() { return telefono; }
-    public void setTelefono(String telefono) { this.telefono = telefono; }
-    public Boolean getClienteFrecuente() { return clienteFrecuente; }
-    public void setClienteFrecuente(Boolean clienteFrecuente) { this.clienteFrecuente = clienteFrecuente; }
 }
