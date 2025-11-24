9828cbf1768c55d6681c50147c1a5545011026a4
Adonis Paniagua <77471071+darkendord@users.noreply.github.com>
2025-11-24 02:06:06 -0600

    Fixed App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,28 @@
-import { RouterProvider } from "react-router-dom";
-import router from "@/infrastructure/router/routes.config.jsx";
+import React from "react";
+import { Outlet } from "react-router-dom";
 import { Toaster } from "@/shared/components/ui/sonner.jsx";
 import { CategoriesProvider } from "./features/categories/context/CategoriesContext";
 import { ProductsProvider } from "./features/products/context/ProductsContext";
+import SideMenu from "@/shared/components/navigation/SideMenu.jsx";
+import { TopBar } from "@/shared/components/layout/TopBar.jsx";
 
 export default function App() {
   return (
     <>
       <CategoriesProvider>
         <ProductsProvider>
-          <RouterProvider router={router} />
+          <div className="app-wrapper mx-auto flex">
+            <SideMenu />
+            <div className="flex-1 min-h-screen bg-white">
+              <TopBar />
+              <main className="p-6">
+                <Outlet />
+              </main>
+            </div>
+          </div>
         </ProductsProvider>
       </CategoriesProvider>
+
       <Toaster />
     </>
   );
