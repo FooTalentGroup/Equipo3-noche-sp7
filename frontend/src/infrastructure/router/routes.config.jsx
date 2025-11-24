36c73051729f7ddd7665ff1505c1beb0c0b1f124
Ricardo Quejuan <quejuan52@gmail.com>
2025-11-24 17:09:08 -0500

    fix(login): Rutas de autenticación correctas para el acceso de inicio de sesión

--- frontend/src/infrastructure/router/routes.config.jsx
+++ frontend/src/infrastructure/router/routes.config.jsx
@@ -10,8 +10,13 @@ import SalesPage from '@/features/sales/pages/SalesPage';
 import DiscountsPage from '@/features/discounts/pages/DiscountPage.jsx';
 import PredictionsPage from '@/features/predictions/pages/PredictionsPage.jsx';
 import ReportsPage from '@/features/repor/pages/ReportsPage.jsx';
+import { authRoutes } from '@/features/auth/routes/auth.routes';
 
 const router = createBrowserRouter([
+     {
+        path: '/',
+        children: authRoutes,
+    },
     {
         path: '/',
         element: <App />,
