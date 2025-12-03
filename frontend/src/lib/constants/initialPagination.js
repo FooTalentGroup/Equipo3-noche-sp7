270b6801f811eba2e4c7355fc80a5f4a9ef75cbc
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-12-02 23:46:00 -0500

    Create initialPagination.js

---
+++ frontend/src/lib/constants/initialPagination.js
@@ -0,0 +1,10 @@
+const INITIAL_PAGINATION_STATE = {
+  currentPage: 0,
+  pageSize: 20,
+  totalElements: 0,
+  totalPages: 0,
+  isFirst: true,
+  isLast: false
+};
+
+export default INITIAL_PAGINATION_STATE;
