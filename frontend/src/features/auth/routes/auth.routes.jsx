640efbc45c3d52baadfcadf1d33c60aeaed7efd9
Ricardo Quejuan <quejuan52@gmail.com>
2025-12-09 14:07:21 -0500

    feat:Agregar ruta para forgot password

--- frontend/src/features/auth/routes/auth.routes.jsx
+++ frontend/src/features/auth/routes/auth.routes.jsx
@@ -1,3 +1,4 @@
+import ForgotPasswordPage from "../pages/ForgotPasswordPage.jsx";
 import LoginPage from "../pages/LoginPage.jsx";
 import RegisterPage from "../pages/RegisterPage.jsx";
 import { PublicRoute } from "@/infrastructure/router/PublicRoute.jsx";
@@ -8,6 +9,14 @@ export const authRoutes = [
     element: (
       <PublicRoute>
         <LoginPage />
+      </PublicRoute>
+    ),
+  },
+   {
+    path: "/forgot-password",
+    element: (
+      <PublicRoute>
+        <ForgotPasswordPage />
       </PublicRoute>
     ),
   },
