fc2249ede4835b019b8cd9fe351f0ef94b9ea1a7
Jorge Trujillo <39315478+xiriuxb@users.noreply.github.com>
2025-12-18 18:48:03 -0500

    Refactor InfoCard and LinkCard UI and props

--- frontend/src/features/home/component/InfoCard.jsx
+++ frontend/src/features/home/component/InfoCard.jsx
@@ -1,17 +1,19 @@
-import { Badge } from "@/shared/components/ui/badge";
+import { Link } from "react-router";
 
-function InfoCard({ title, badgeIcon: Icon, quantity, description, className }) {
+function InfoCard({ title, href, badgeIcon: Icon, quantity, description, className }) {
   return (
-    <div className={`max-h-44 flex flex-col gap-4 p-6 border border-stokia-neutral-200 rounded-xl bg-stokia-neutral-50 ${className}`}>
-      <div className="flex flex-col gap-2.5">
-        <Badge variant={"secondary"}>
-          <Icon />
+    <Link to={href} className={`group max-h-48 flex flex-col gap-4 p-6 border border-stokia-neutral-200 rounded-xl bg-stokia-neutral-50 shadow-lg hover:bg-stokia-neutral-300/20 transition-all ${className}`}>
+      <div className="flex flex-col gap-2">
+        <div className=" flex justify-between text-xl items-center gap-2">
           {title}
-        </Badge>
+          <span className="p-2.5 bg-stokia-primary-200 rounded-xl group-hover:shadow-sm transition-shadow">
+            <Icon className="text-stokia-primary-600" />
+          </span>
+        </div>
         <p className="text-5xl font-bold text-stokia-neutral-950">{quantity}</p>
       </div>
       <p className="font-medium text-stokia-neutral-950">{description}</p>
-    </div>
+    </Link>
   )
 }
 
--- frontend/src/features/home/component/LinkCard.jsx
+++ frontend/src/features/home/component/LinkCard.jsx
@@ -1,16 +1,14 @@
 import { Link } from "react-router";
 
-function LinkCard({ image, title, icon: Icon, className, href }) {
+function LinkCard({ title, icon: Icon, className, href, description }) {
   return (
-    <Link to={href} className={`group relative max-h-[240px] border border-stokia-neutral-200 rounded-xl overflow-hidden transition-all cursor-pointer bg-stokia-neutral-50 ${className}`}>
-      <img
-        src={image}
-        alt="Dashboard Link Card"
-        className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
-      />
-      <div className="flex items-center w-full gap-3 p-5 bg-stokia-neutral-50 absolute bottom-0">
-        <Icon className="text-stokia-neutral-700" size={20} />
-        <p className="font-medium text-stokia-neutral-900 text-lg">{title}</p>
+    <Link to={href} className={`flex flex-col group items-start gap-4 max-h-60 border border-stokia-neutral-200 rounded-xl overflow-hidden transition-all cursor-pointer bg-stokia-neutral-50 shadow-lg p-6 hover:bg-stokia-neutral-300/20 ${className}`}>
+      <span className="p-3 bg-stokia-primary-200 rounded-xl group-hover:transform group-hover:shadow-md transition-shadow">
+        <Icon className="text-stokia-primary-600" size={22} />
+      </span>
+      <div className="flex flex-col w-full gap-3 bottom-0">
+        <p className="font-medium text-stokia-neutral-900 text-xl">{title}</p>
+        <p className="text-stokia-neutral-950 text-base">{description ?? "Ver más"}</p>
       </div>
     </Link>
   );
